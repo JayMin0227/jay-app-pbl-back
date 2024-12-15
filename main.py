880ccf116726fbbe51a67b76cbb69b4e4d3edf0b
JayMin0227 <ryosakuma0227@gmail.com>
2024-12-16 02:00:50 +0900

    main.py changed

--- main.py
+++ main.py
@@ -91,7 +91,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://jay-app-pbl-back.vercel.app"],
+    # allow_origins=["http://localhost:3000", "https://jay-app-pbl-back.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
